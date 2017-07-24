b493a41a0d70e49493700751c6cf664a1dde6fa1
ray coti <Raymond.coti@gmail.com>
2017-07-24 16:42:02 -0500

    adds react components

--- client/constants.js
+++ client/constants.js
@@ -6,9 +6,3 @@ export const SET_CURRENT = 'SET_CURRENT';
 export const LOAD_BLOGS = 'LOAD_BLOGS';
 export const SET_NAME = 'SET_NAME';
 export const DELETE_BLOG = 'DELETE_BLOG';
-
-
-
-0bb4:0c81
-
-echo SUBSYSTEM=="usb", ATTR{idVendor}=="0bb4", ATTR{idProduct}="0c81" MODE="0666", GROUP="plugdev" | sudo tee /etc/udev/rules.d/51-android-usb.rules
---
+++ client/containers/index.js
@@ -0,0 +1,3 @@
+export {default as bloggerContainer } from './bloggerContainer';
+export {default as blogsContainer } from './blogsContainer';
+export {default as blogPostContainer } from './blogPostContainer';
