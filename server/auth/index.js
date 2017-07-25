08d216b0099ee78214e40caa36655f6475fdbedf
ray coti <Raymond.coti@gmail.com>
2017-07-25 11:30:28 -0500

    returns promises

--- server/auth/index.js
+++ server/auth/index.js
@@ -6,11 +6,11 @@ router.post('/login', (req, res, next) => {
   User.findOne({where: {email: req.body.email}})
     .then(user => {
       if (!user) {
-        res.status(401).send('User not found')
+        return res.status(401).send('User not found')
       } else if (!user.correctPassword(req.body.password)) {
-        res.status(401).send('Incorrect password')
+        return res.status(401).send('Incorrect password')
       } else {
-        req.login(user, err => err ? next(err) : res.json(user))
+        return req.login(user, err => err ? next(err) : res.json(user))
       }
     })
     .catch(next)
@@ -19,11 +19,11 @@ router.post('/login', (req, res, next) => {
 router.post('/signup', (req, res, next) => {
   User.create(req.body)
     .then(user => {
-      req.login(user, err => err ? next(err) : res.json(user))
+      return req.login(user, err => err ? next(err) : res.json(user))
     })
     .catch(err => {
       if (err.name === 'SequelizeUniqueConstraintError')
-        res.status(401).send('User already exists')
+        return res.status(401).send('User already exists')
       else next(err)
     })
 })
